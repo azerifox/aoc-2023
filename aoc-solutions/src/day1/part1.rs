f33d6f617a2d9784b94412bc5add25be96a70f35
azerifox <azerifox.dev@outlook.com>
2023-12-03 03:13:55 +0100

    Day 1 part 1 solved

--- aoc-solutions/src/day1/part1.rs
+++ aoc-solutions/src/day1/part1.rs
@@ -1,3 +1,50 @@
 pub fn solve(input: &str) -> Result<String, String> {
-    Ok(String::from("Day1 part 1 todo"))
+    let lines: Vec<&str> = input.split("\n").collect();
+
+    let mut calibration_values: Vec<u32> = Vec::new();
+
+    for line in lines {
+        if line.is_empty() {
+            continue;
+        };
+
+        let first_digit = find_first_digit(line)?;
+        let last_digit = find_last_digit(line)?;
+
+        let line_value = first_digit.to_string() + &last_digit.to_string();
+        let line_value: u32 = line_value
+            .parse()
+            .map_err(|_| "the line value should be parsable into a number".to_string())?;
+
+        calibration_values.push(line_value);
+    }
+
+    let sum: u32 = calibration_values.iter().sum();
+    Ok(sum.to_string())
+}
+
+fn find_first_digit(input_line: &str) -> Result<char, String> {
+    for character in input_line.chars() {
+        match character.is_digit(10) {
+            true => return Ok(character),
+            false => continue,
+        };
+    }
+
+    Err(String::from(
+        "Didn't find any digit, there should have been at least one",
+    ))
+}
+
+fn find_last_digit(input_line: &str) -> Result<char, String> {
+    for character in input_line.chars().rev() {
+        match character.is_digit(10) {
+            true => return Ok(character),
+            false => continue,
+        };
+    }
+
+    Err(String::from(
+        "Didn't find any digit, there should have been at least one",
+    ))
 }
